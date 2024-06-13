a89af28020de0f791a5d85f79ec4de17ccf70aeb
keshabbhatt <keshabrajbhatt12345@gmail.com>
2024-06-13 22:29:04 +0545

    kunssl

---
+++ routes/User.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  UserLogin,
+  UserRegister,
+} from "../controllers/User.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+router.post("/signup", UserRegister);
+router.post("/signin", UserLogin);
+
+
+export default router;
