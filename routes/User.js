38db64a67db753df2dfc4a0f5d4f17207a7f4c13
keshabbhatt <keshabrajbhatt12345@gmail.com>
2024-06-14 00:44:06 +0545

    backend modified

--- routes/User.js
+++ routes/User.js
@@ -1,14 +1,17 @@
-import express from "express";
+// Importing required modules and functions
+import express from "express"; // Importing Express framework for creating server and handling routes
 import {
-  UserLogin,
-  UserRegister,
-} from "../controllers/User.js";
-import { verifyToken } from "../middleware/verifyToken.js";
+  UserLogin, // Importing UserLogin controller function
+  UserRegister, // Importing UserRegister controller function
+} from "../controllers/User.js"; // Importing from the User controller
+import { verifyToken } from "../middleware/verifyToken.js"; // Importing middleware to verify JWT tokens
 
-const router = express.Router();
+// Create a new router object
+const router = express.Router(); // Creating an instance of an Express Router
 
-router.post("/signup", UserRegister);
-router.post("/signin", UserLogin);
+// Define route for user registration
+router.post("/signup", UserRegister); // POST request to /api/user/signup will invoke UserRegister controller
 
-
-export default router;
+// Define route for user login
+router.post("/signin", UserLogin); // POST request to /api/user/signin will invoke UserLogin controller
+export default router; // Exporting the router to be used in other parts of the application
